dac0c930b4358babeda1a43ac56f264b4f7bdf12
Ariel Herman <aeh.herman@gmail.com>
2018-04-11 19:41:14 -0400

    5 tests passing for checkForShip

--- game_logic/ship_methods.js
+++ game_logic/ship_methods.js
@@ -1,17 +1,18 @@
 const checkForShip = (player, coords) => {
-  let shipPresent;
-  player.ships.forEach(ship => {
+  let shipPresent, ship;
+  for (let i = 0; i < player.ships.length; i++) {
+    ship = player.ships[i];
+
     shipPresent = ship.locations.find(
       actualCoords =>
         actualCoords[0] === coords[0] && actualCoords[1] === coords[1]
     );
-  });
-
-  if (!shipPresent) {
-    return false;
-  } else {
-    return true;
+    if (shipPresent) {
+      return true;
+    }
   }
+
+  return false;
 };
 
 module.exports.checkForShip = checkForShip;
--- test/ship_test.js
+++ test/ship_test.js
@@ -15,11 +15,29 @@ describe("checkForShip", () => {
   });
 
   it("should correctly report a ship located at a given player's coordinate", () => {
-    // initially, make an expectation about what parameters your fn will take
-    // and what type they are
     player = {
       ships: [{ locations: [[0, 0]] }]
     };
     expect(checkForShip(player, [0, 0])).to.be.true;
+  });
+
+  it("should handle ships located at more than one coordinate", () => {
+    player = {
+      ships: [{ locations: [[0, 0], [0, 1]] }]
+    };
+    expect(checkForShip(player, [0, 0])).to.be.true;
+    expect(checkForShip(player, [0, 1])).to.be.true;
+    expect(checkForShip(player, [9, 9])).to.be.false;
+  });
+
+  it("should handle multiple ships", () => {
+    player = {
+      ships: [{ locations: [[0, 0], [0, 1]] }, { locations: [[3, 5], [4, 5]] }]
+    };
+    expect(checkForShip(player, [0, 0])).to.be.true;
+    expect(checkForShip(player, [0, 1])).to.be.true;
+    expect(checkForShip(player, [3, 5])).to.be.true;
+    expect(checkForShip(player, [4, 5])).to.be.true;
+    expect(checkForShip(player, [9, 9])).to.be.false;
   });
 });
